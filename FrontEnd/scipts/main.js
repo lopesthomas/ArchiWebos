a09614c00d945c2bf770153cb61806079f8ba5d3
Thomas Lopes <thomaslopes001@gmail.com>
2023-03-19 23:15:22 +0100

    Affichage avec la fonction genererProjet()

--- FrontEnd/scipts/main.js
+++ FrontEnd/scipts/main.js
@@ -8,11 +8,44 @@ async function Init(Valeur){
     console.log(Projet);
   
     tableau(Projet);
-    genererProjet(Projet);
+    genererProjet(donneesProjets);
   
     return Valeur = Projet;
   };
+let donneesProjets;
 Init();
+
+//Tranforme les donnees Projets en un tableau
+function tableau(Projet){donneesProjets = Projet; console.log(donneesProjets)};
+
+function genererProjet(donneesProjets){
+    document.querySelector(".gallery").innerHTML = "";
+      for (let i = 0; i < donneesProjets.length; i++) {
+    
+        
+        const figure = donneesProjets[i];
+        // Récupération de l'élément du DOM qui accueillera les projets
+        const sectionFiches = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un projet
+        const pieceElement = document.createElement("figure");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+        const nomElement = document.createElement("figcaption");
+        nomElement.innerText = figure.title;
+      
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = figure.category.id ?? "(aucune catégorie)";
+        
+        // On rattache la balise figure a la class gallery
+        sectionFiches.appendChild(pieceElement);
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(categorieElement);
+    
+      }
+       
+    };
 
 
 
