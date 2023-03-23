3a8ab8f195d94782abf31fae8715494beecb81f3
Thomas Lopes <thomaslopes001@gmail.com>
2023-03-23 23:45:50 +0100

    Indentation des scripts

--- FrontEnd/scipts/main.js
+++ FrontEnd/scipts/main.js
@@ -1,35 +1,27 @@
+//Recuperation des donnees des Projets serveur
+function init(){
+    console.log("Initialisation");
+  
+    fetch('http://localhost:5678/api/works')
+    .then(response =>(response.json()))
+    .then(data => sendData(data));
+};
+init();
+    
+function sendData(donneesProjets) {
 
-    //Recuperation des donnees des Projets serveur
-    async function init(){
-        console.log("Initialisation");
-      
-        fetch('http://localhost:5678/api/works')
-        .then(response =>(response.json()))
-        .then(data => sendData(data));
-      
+    genererProjet(donneesProjets);
+    genererButtons(donneesProjets);
+}
     
-      
+function genererProjet(donneesProjets, x){
     
-      };
-    init();
-    
-    function sendData(donneesProjets) {
-    
-     genererProjet(donneesProjets);
-     genererButtons(donneesProjets);
-     }
-    
-    
-    
-    function genererProjet(donneesProjets, x){
-    
-        if(x > 0) {
-            console.log("valeur de x ", x);
-            document.querySelector(".gallery").innerHTML = "";
-            for (let i = 0; i < donneesProjets.length; i++) {
-                if(x == donneesProjets[i].category.id){
-        
-            
+    if(x > 0) {
+        console.log("valeur de x ", x);
+        document.querySelector(".gallery").innerHTML = "";
+        for (let i = 0; i < donneesProjets.length; i++) {
+            if(x == donneesProjets[i].category.id){
+                    
                 const figure = donneesProjets[i];
                 // Récupération de l'élément du DOM qui accueillera les projets
                 const sectionFiches = document.querySelector(".gallery");
@@ -48,16 +40,13 @@
                 sectionFiches.appendChild(pieceElement);
                 pieceElement.appendChild(imageElement);
                 pieceElement.appendChild(nomElement);
-                pieceElement.appendChild(categorieElement);
-            
-                }
-              }
-    
-        } else {
+                pieceElement.appendChild(categorieElement);        
+            }
+        }
+    } else {
     
         document.querySelector(".gallery").innerHTML = "";
-          for (let i = 0; i < donneesProjets.length; i++) {
-        
+        for (let i = 0; i < donneesProjets.length; i++) {        
             
             const figure = donneesProjets[i];
             // Récupération de l'élément du DOM qui accueillera les projets
@@ -77,73 +66,56 @@
             sectionFiches.appendChild(pieceElement);
             pieceElement.appendChild(imageElement);
             pieceElement.appendChild(nomElement);
-            pieceElement.appendChild(categorieElement);
-        
-            
-          }
-         }
+            pieceElement.appendChild(categorieElement);            
+        }
+    }
+};
+    
+const setDonnees = new Set();    
+    
+function genererButtons(donneesProjets){
+    
+    //Cible la balise pour inserer les buttons
+    const cibleHTML = document.querySelector(".buttons");  
+    //Creation du button
+    var button = document.createElement("button");
+    button.innerHTML = "Tous";
+    //Placer en element "Child" a la cible
+    cibleHTML.appendChild(button);
+    //Evenement au Click du button
+    button.addEventListener ("click", function() {
+        // alert("alert");
+        genererProjet(donneesProjets);
+        console.log("button click");
+    });
+                    
+    for (let i = 0; i < donneesProjets.length; i++) {
+        let catname = donneesProjets[i].category.name;
+        setDonnees.add(catname);
+        console.log(setDonnees);
+        // console.log(i);
+    };
+
+    const setIter = setDonnees[Symbol.iterator]();
+    let id = 0;
+
+    function generateClickFunction(buttonNumber) {
+        return function() {
+            genererProjet(donneesProjets, buttonNumber)
+            console.log("Vous avez cliqué sur le bouton " + buttonNumber);
         };
-    
-        const setDonnees = new Set();    
-    
-        function genererButtons(donneesProjets){
-    
-            //Cible la balise pour inserer les buttons
-            const cibleHTML = document.querySelector(".buttons");  
-            //Creation du button
-            var button = document.createElement("button");
-            button.innerHTML = "Tous";
-            //Placer en element "Child" a la cible
-            cibleHTML.appendChild(button);
-            //Evenement au Click du button
-            button.addEventListener ("click", function() {
-             // alert("alert");
-              genererProjet(donneesProjets);
-              console.log("button click");
-            });
-          
-          
-          
-          
-            for (let i = 0; i < donneesProjets.length; i++) {
-              let catname = donneesProjets[i].category.name;
-              setDonnees.add(catname);
-              console.log(setDonnees);    
-          
-              // console.log(i);
-          
-            };
-    
-            const setIter = setDonnees[Symbol.iterator]();
-            let id = 0;
-    
-            function generateClickFunction(buttonNumber) {
-                return function() {
-                    genererProjet(donneesProjets, buttonNumber)
-                  console.log("Vous avez cliqué sur le bouton " + buttonNumber);
-                };
-              }
-    
-          //  for (let j = 0; j < set1.size; j++) 
-            setDonnees.forEach(function(value) {
-              let recup = setIter.next().value;
-              
-              id = id + 1;  
-              button = document.createElement("button");
-              button.innerHTML = recup;
-              button.className = id; 
-              button.addEventListener("click", generateClickFunction(id));
-    
-              cibleHTML.appendChild(button);
-    
-              
-    
-              console.log(" valeur ", );
-          
-            });
-          
-          }
-    
-    
-    
-      
+    }
+
+    //  for (let j = 0; j < set1.size; j++) 
+    setDonnees.forEach(function(value) {
+        let recup = setIter.next().value;
+
+        id = id + 1;
+        button = document.createElement("button");
+        button.innerHTML = recup;
+        button.className = id;
+        button.addEventListener("click", generateClickFunction(id));
+
+        cibleHTML.appendChild(button);
+    });
+}
